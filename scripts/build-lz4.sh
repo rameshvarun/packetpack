b7596b38f8dd1a8507eff9976c9bc1aae70cdd22
Varun Ramesh <varunramesh@live.com>
2023-04-04 19:13:17 -0700

    Assume web environment so we don't try to laod fs

--- scripts/build-lz4.sh
+++ scripts/build-lz4.sh
@@ -11,4 +11,5 @@ emcc lz4/lib/lz4.c -O3 \
     -s EXPORTED_FUNCTIONS="['_malloc','_LZ4_createStream', '_LZ4_decoderRingBufferSize', '_LZ4_compressBound', '_LZ4_compress_fast_continue', '_LZ4_decompress_safe_continue']" \
     -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap']" \
     -s SINGLE_FILE=1 \
+    -s ENVIRONMENT=web \
     -o src/lz4.js
